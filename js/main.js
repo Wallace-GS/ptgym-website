469f0b46014e2e14a1722be1adb86dd5700aac8f
Wallace-GS <wgsantos29@gmail.com>
2019-03-16 15:05:26 -0700

    Add #contact, Maps API, footer, smooth scrolling

---
+++ js/main.js
@@ -0,0 +1,24 @@
+function initMap() {
+    const loc = { lat: 37.773972, lng: -122.431297 };
+    const map = new google.maps.Map(document.querySelector('.map')
+    , {
+        zoom: 12,
+        center: loc
+    });
+    const marker = new google.maps.Marker({ position: loc, map: map });
+}
+
+$('#navbar a, .btn').on('click', function(event) {
+    if (this.hash != '') {
+        event.preventDefault();
+
+        const hash = this.hash;
+
+        $('html, body').animate(
+            {
+                scrollTop: $(hash).offset().top - 43
+            },
+            800
+        );
+    }
+});
